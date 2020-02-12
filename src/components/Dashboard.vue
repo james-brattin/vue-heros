93d3b653be3b547e10368b0a464742cbe3163c9a
James Brattin <james.brattin@gmail.com>
2020-02-11 19:23:13 -0600

    Added error template and changed url to use lambda heros api

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,25 @@
 <template>
+<div>
   <ul v-if="heros && heros.length">
     <li v-for="hero of heros" v-bind:key="hero.id">
       <p><strong>{{hero.name}}</strong></p>
       <p>{{hero.description}}</p>
     </li>
   </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors" v-bind:key="error.id">
+      {{error.message}}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
 import {HTTP} from './http-common';
 
 export default {
+    name: 'Dashboard',
     data() {
         return {
             heros: [],
--- src/components/http-common.js
+++ src/components/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `https://71uxr7sasa.execute-api.us-east-2.amazonaws.com/beta`,
-  headers: {
-    Authorization: 'Bearer {token}'
-  }
+  baseURL: `https://71uxr7sasa.execute-api.us-east-2.amazonaws.com/beta`
+  // headers: {
+  //   Authorization: 'Bearer {token}'
+  // }
 })
