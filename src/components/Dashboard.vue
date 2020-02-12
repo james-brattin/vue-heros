972f594fcec756775059c1eb3ee823b45687cc3d
James Brattin <james.brattin@gmail.com>
2020-02-11 19:07:34 -0600

    added a http common file, dashboard component which loops through a heros array

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul v-if="heros && heros.length">
+    <li v-for="hero of heros" v-bind:key="hero.id">
+      <p><strong>{{hero.name}}</strong></p>
+      <p>{{hero.description}}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+import {HTTP} from './http-common';
+
+export default {
+    data() {
+        return {
+            heros: [],
+            errors: []
+        }
+    },
+    created() {
+        HTTP.get(`heros`)
+            .then(response => {
+                this.heros = response.data
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+    }
+}
+</script>
+
+<style>
+
+</style>
