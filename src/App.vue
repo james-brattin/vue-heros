ac82f163b24b49030236ce7f661a01056cc0506d
james-brattin <james.brattin@gmail.com>
2021-05-01 20:11:24 +0200

    fixed formatting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -24,55 +21,42 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
+    <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col class="text-center">
-            <Dashboard/>
+            <Dashboard />
           </v-col>
         </v-row>
       </v-container>
     </v-content>
-    <v-footer
-      color="indigo"
-      app
-    >
+    <v-footer color="indigo" app>
       <span class="white--text">&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import Dashboard from './components/Dashboard.vue'
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Dashboard
+    Dashboard,
   },
   props: {
-      source: String
-      },
+    source: String,
+  },
   data: () => ({
     drawer: false,
   }),
-  created () {
-      this.$vuetify.theme.dark = true
-    }
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
 };
 </script>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,54 +1,48 @@
 <template>
-<div>
-  <div v-if="heros && heros.length">
-    <div v-for="hero of heros" v-bind:key="hero.id">
-      <v-card
-      class="mx-auto"
-      max-width="344"
-      outlined
-      >
-      <v-card-title>{{hero.name}}</v-card-title>
-      <v-card-text>{{hero.description}}</v-card-text>
+  <div>
+    <div v-if="heros && heros.length">
+      <div v-for="hero of heros" v-bind:key="hero.id">
+        <v-card class="mx-auto" max-width="344" outlined>
+          <v-card-title>{{ hero.name }}</v-card-title>
+          <v-card-text>{{ hero.description }}</v-card-text>
 
-      <v-card-actions>
-        <v-btn text>Edit</v-btn>
-        <v-btn text>Delete</v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
-  </div>
+          <v-card-actions>
+            <v-btn text>Edit</v-btn>
+            <v-btn text>Delete</v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </div>
 
-  <ul v-if="errors && errors.length">
-    <li v-for="error of errors" v-bind:key="error.id">
-      {{error.message}}
-    </li>
-  </ul>
-</div>
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors" v-bind:key="error.id">
+        {{ error.message }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import {HTTP} from './http-common';
+import { HTTP } from "./http-common";
 
 export default {
-    name: 'Dashboard',
-    data() {
-        return {
-            heros: [],
-            errors: []
-        }
-    },
-    created() {
-        HTTP.get(`heros`)
-            .then(response => {
-                this.heros = response.data
-            })
-            .catch(e => {
-                this.errors.push(e)
-            })
-    }
-}
+  name: "Dashboard",
+  data() {
+    return {
+      heros: [],
+      errors: [],
+    };
+  },
+  created() {
+    HTTP.get(`heros`)
+      .then((response) => {
+        this.heros = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
